f3823942147b404b752d3ed7c6e7da7e93a4d412
Sindre-Espedal <89195704+Sindre-Espedal@users.noreply.github.com>
2021-10-13 13:41:16 +0200

    Mapper lagt inn

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -13,6 +13,17 @@
 #   Check Package:             'Cmd + Shift + E'
 #   Test Package:              'Cmd + Shift + T'
 
-hello <- function() {
-  print("Hello, world!")
+
+hello <- function(name=NULL) {
+  tekst <- paste("Hello world! My name is", name)
+  print(tekst)
 }
+
+hello("Sindre")
+
+#' This is a hello function with a name argument #' @export
+hello <- function() {
+  print("Hello, world! My name is Sindre")
+}
+
+
